67a1569857f22d875ca53434330e98653f64d110
Nanthawat Duang-ead <33195267+D7NAMITE@users.noreply.github.com>
2025-04-18 22:45:31 +0700

    Initialize Project with DaisyUI

---
+++ todo-application/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="navbar bg-base-100 shadow-sm">>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+        </header>
+    )
+}
